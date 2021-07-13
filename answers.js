3e5f73587bc421c1075e30f9b1dabda2801568e6
Neet <navneetkaur3@gmail.com>
2021-07-13 18:42:25 -0500

    "datatype refresher complete"

---
+++ answers.js
@@ -0,0 +1,55 @@
+// A light switch that can be either on or off.
+//This will be a boolean variable with values on and off.
+let lightSwitchOn = new Boolean(true)
+Let lightSwitchOff = new Boolean(false)
+
+// A user's email address.
+//This will be a string variable
+let userEmailAddress = "xyz@gmail.com"
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//This will be object variable
+const spaceship = {
+  hull: true,
+  laserBlaster: true,
+  tractorBeam: true,
+  wrapDrive: true}
+// A list of student names from our class.
+//Thiis will be type array
+let nameList = ['name1', 'name2']
+
+// A list of student names from our class, each with a location.
+//Array of objects
+let StudentList =[
+  {
+    "name": "student1",
+    "location":"location 1"
+  },
+  {
+    "name": "student2",
+    "location":"location 2"
+  },
+  {
+    "name": "student3",
+    "location":"location 3"
+  }]
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//array of objects with one object property being Array
+
+  let StudentList =[
+    {
+      "name": "student1",
+      "location":"location 1"
+      "favShows":["show1","show2"]
+
+    },
+    {
+      "name": "student2",
+      "location":"location 2"
+      "favShows":["show11","show22"]
+    },
+    {
+      "name": "student3",
+      "location":"location 3"
+      "favShows":["show111","show222"]
+    }]
